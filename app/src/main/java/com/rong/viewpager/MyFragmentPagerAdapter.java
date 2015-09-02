631530a8e7e9c746da975b0ea6b38a6bbe0ee654
nico <huangwenzeng1@163.com>
2015-09-02 18:47:14 +0800

    no message

---
+++ app/src/main/java/com/rong/viewpager/MyFragmentPagerAdapter.java
@@ -0,0 +1,32 @@
+package com.rong.viewpager;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.ArrayList;
+
+public class MyFragmentPagerAdapter extends FragmentStatePagerAdapter{
+
+    ArrayList<Fragment> list;
+    public MyFragmentPagerAdapter(FragmentManager fm,ArrayList<Fragment> list) {
+        super(fm);
+        this.list = list;
+
+    }
+
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Fragment getItem(int arg0) {
+        return list.get(arg0);
+    }
+
+
+
+
+}
